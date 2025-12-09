34b2deccda4e79327c6683bf4379a99fa35f25c3
Grace N. Marin Estrada <GNestrada32@gmail.com>
2025-12-09 18:45:58 -0400

    Add SimpleMDB Landing Page

--- src/Smdb.Csr/App.cs
+++ src/Smdb.Csr/App.cs
@@ -2,26 +2,29 @@ namespace Smdb.Csr;
 
 using Shared.Http;
 
+//Tiene la funcionalidad de crear un Http listener en el puerto y 
+//en el host indicado en appsetting.cfg
 public class App : HttpServer
 {
 	public App()
 	{
 	}
 
+	//Se rescribe el metodo Init para ensamblar nuestro App
 	public override void Init()
 	{
 		router.Use(HttpUtils.StructuredLogging);
 		router.Use(HttpUtils.CentralizedErrorHandling);
 		router.Use(HttpUtils.AddResponseCorsHeaders);
-		router.Use(HttpUtils.DefaultResponse);
-		router.Use(HttpUtils.ParseRequestUrl);
-		router.Use(HttpUtils.ParseRequestQueryString);
-		router.Use(HttpUtils.ServeStaticFiles);
-		router.UseSimpleRouteMatching();
+		router.Use(HttpUtils.DefaultResponse);//Por si se intenta acceder a una ruta que no existe (404)
+		router.Use(HttpUtils.ParseRequestUrl);//Obsoleto, C# ya lo hace default
+		router.Use(HttpUtils.ParseRequestQueryString);//Obsoleto, C# ya lo hace default
+		router.Use(HttpUtils.ServeStaticFiles);//Lo unico que hace es servir Static files
+		router.UseSimpleRouteMatching();//Routing machine de MapGet
 
-		router.MapGet("/", async (req, res, props, next) =>
-			{ res.Redirect("/index.html"); await next(); });
-		router.MapGet("/movies", async (req, res, props, next) =>
-		{ res.Redirect("/movies/index.html"); await next(); });
+		//Cuando se quiera acceder al folder de landing page o movies sin poner /index.html 
+		//se le redirige para que la pueda acceder
+		router.MapGet("/", async (req, res, props, next) => { res.Redirect("/index.html"); await next(); });
+		router.MapGet("/movies", async (req, res, props, next) => { res.Redirect("/movies/index.html"); await next(); });
 	}
 }
